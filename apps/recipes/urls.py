edd3b85c78ab1da3f9667f2dc8c03c4895112f27
michal <karalusmichal@gmail.com>
2020-12-16 23:37:14 +0100

    Hot fix

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.RecipesView.as_view(), name='recipes'),
     path('<uuid:pk>', views.RecipeView.as_view(), name='recipe'),
     path('add-recipe/', views.AddRecipeView.as_view(), name='add_recipe'),
-    path('<slug:slug>/update', views.UpdateRecipeView.as_view(), name='update_recipe'),
-    path('<slug:slug>/delete', views.DeleteRecipeView.as_view(), name='delete_recipe')
+    path('<uuid:pk>/update', views.UpdateRecipeView.as_view(), name='update_recipe'),
+    path('<uuid:pk>/delete', views.DeleteRecipeView.as_view(), name='delete_recipe')
 ]
