ba13b4a478fa160806957e25a9137e2776002304
michal <karalusmichal@gmail.com>
2021-01-12 21:34:15 +0100

    Add signup view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .forms import UserCreationForm
+
+
+class SignUpView(generic.CreateView):
+
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = '/login/'
