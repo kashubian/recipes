8a854d033093ff9bd103680cd902f497c7b84926
michal <karalusmichal@gmail.com>
2021-01-14 22:49:17 +0100

    Update change password form

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('change-password/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('accounts:password_change_done')), name='change_password'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name="accounts/password_change_form.html", success_url=reverse_lazy('accounts:password_change_done')), name='change_password'),
     path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="accounts/password_change_done.html"), name='password_change_done')
 ]
